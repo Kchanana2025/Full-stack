2957c4a793fc92b9f90d8f65cbae69f6d88195ee
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-08 15:51:12 +0530

    added two files

--- create_pattern(rows).js
+++ create_pattern(rows).js
@@ -1,9 +1,9 @@
-function create_pattern(rows){
-  for(i=1;i<=rows;i++){
+function create_pattern(rows) {
+  for (i = 1; i <= rows; i++) {
     console.log(i);//when we write console.log(anything) we automattically shift to next line after something is printed
-    var j=i;
-    while(j){ 
-    process.stdout.write('*');//we do not shift to next line by process.stdout.write()
+    var j = i;
+    while (j) {
+      process.stdout.write('*');//we do not shift to next line by process.stdout.write()
       j--;
     }
     console.log();//to delibrately move to the next line we can write console.log() 
--- passing_function_inside_function.js
+++ passing_function_inside_function.js
@@ -1,20 +1,20 @@
 
-function sum(val1,val2){
-  return val1+val2;
+function sum(val1, val2) {
+  return val1 + val2;
 }
-function subs(val1,val2){
-  return val1-val2;
+function subs(val1, val2) {
+  return val1 - val2;
 }
-function mul(val1,val2){
-  return val1*val2;
+function mul(val1, val2) {
+  return val1 * val2;
 }
-function div(val1,val2){
-  return val1/val2;
+function div(val1, val2) {
+  return val1 / val2;
 }
-function doarithmetic(val1,val2,arithmetic)//no need to mention the data type of variables you are accepting
-{ 
-  return arithmetic(val1,val2);
+function doarithmetic(val1, val2, arithmetic)//no need to mention the data type of variables you are accepting
+{
+  return arithmetic(val1, val2);
 }
 
-var ans=doarithmetic(2,3,sum);
+var ans = doarithmetic(2, 3, sum);
 console.log(ans);
